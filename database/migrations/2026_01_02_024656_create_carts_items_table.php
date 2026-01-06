a7a31df06d2f22db5940ee876ef9d96ea42c43e6
Farhan Nugraha <farhan.14092003@gmail.com>
2026-01-06 09:39:15 +0700

    (fix): remove double migration notes coloumn

--- database/migrations/2026_01_02_024656_create_carts_items_table.php
+++ database/migrations/2026_01_02_024656_create_carts_items_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->foreignId('menu_id')->constrained('menus')->onDelete('cascade');
             $table->integer('quantity');
             $table->decimal('price', 10, 2)->comment('harga saat ditambahkan');
-            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
